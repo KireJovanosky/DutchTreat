efa9029487932d1cb86a59e880fffa2b33bc58f7
Kire.Jovanoski <kire.jovanosky@gmail.com>
2024-03-18 09:54:30 +0100

    jquery code added

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -15,5 +15,15 @@ button.on("click", function () {
 var productInfo = $(".product-props li");
 productInfo.on("click", function () {
     console.log("You clicked on " + $(this).text());
+});
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.toggle();
     });
+
+
+
 });
