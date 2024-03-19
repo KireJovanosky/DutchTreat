91c55535f7d91fd096cb3958683fe026e29ff05e
Kire.Jovanoski <kire.jovanosky@gmail.com>
2024-03-19 10:00:25 +0100

    MVC enabled, contact and about us pages added

---
+++ Controllers/AppController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchCore.Controllers
+{
+    public class AppController : Controller 
+    {
+        public IActionResult Index()
+        {
+            //throw new InvalidProgramException("Bad thing happen to good programmers");
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About";
+
+            return View();
+        }
+    }
+}
